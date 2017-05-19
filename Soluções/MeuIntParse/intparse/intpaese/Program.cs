8985cb943a7fa0893daa37e2a0af3ca4f47e5a9c
Ana Carolina <werneckbsc@gmail.com>
2017-05-19 17:20:57 -0300

    intparse não acabado

---
+++ Soluções/MeuIntParse/intparse/intpaese/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intpaese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 * MeuIntParse(Console.ReadLine()));
+        }
+
+        public static int MeuIntParse(string nroComoStr)
+        {
+	     
+	
+	
+        }
+    }
+    }
+}
