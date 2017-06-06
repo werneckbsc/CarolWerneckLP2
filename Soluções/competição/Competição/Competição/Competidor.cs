a2983efddd17511251af309186a855a6140837a8
CarolWerneckLP2 <werneckbsc@gmail.com>
2017-06-06 15:56:54 -0300

    competidor

---
+++ Soluções/competição/Competição/Competição/Competidor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competição
+{
+    class Competidor
+    {
+        public string nome;
+        public double t1, t2, t3;
+
+        public double TempoOficial()
+        {
+
+            if (t1 > t2 && t1 > t3)
+                return t2 + t3;
+            else if (t2 > t1 && t2 > t3)
+                return t1 + t3;
+            else
+                return t2 + t1;
+        }
+
+    }
+}
---
+++ Soluções/competição/Competição/Competição/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double t1, t2, t3;
+            int n, i;
+            
+            Console.WriteLine("Quantos competidores?");
+            n = int.Parse(Console.ReadLine());
+              Competidor[] corredor = new Competidor [i];
+            for (i = 0; i < n; i++)
+            {
+            Console.WriteLine("Informe o nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine ("Informe o 1º tempo:");
+            t1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o 2º tempo:");
+            t2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o 3º tempo:");
+            t3 = Convert.ToDouble(Console.ReadLine());
+             }
+
+
+             
+           
+        }
+    }
+}
