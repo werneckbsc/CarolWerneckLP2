80c43db76107f220fe7302f75b79a8253842422b
Ana Carolina <werneckbsc@gmail.com>
2017-06-13 21:07:17 -0300

    MeuParse

--- Soluções/MeuParse/Meu Parse/Meu Parse/Properties/AssemblyInfo.cs
+++ Soluções/MeuParse/Meu Parse/Meu Parse/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("intpaese")]
+[assembly: AssemblyTitle("Meu Parse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("intpaese")]
+[assembly: AssemblyProduct("Meu Parse")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d8c76813-c523-4618-9e33-53378771145c")]
+[assembly: Guid("5f412c46-1fea-44b3-bfff-988cb61ceeb8")]
 
 // Version information for an assembly consists of the following four values:
 //
